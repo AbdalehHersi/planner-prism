3debd21d3ba0bf89d5f5b1fb70231fa2c726f71c
Abdaleh Hersi <therealhersi03@gmail.com>
2022-08-02 12:36:05 +0100

    added functioning timeblocks. added past/present/future indicator.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var today = moment();
+var currentHour = moment().hours();
+console.log(currentHour);
+
 var update = function() {
     document.getElementById("currentDay")
     .textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -13,4 +15,29 @@ function showText(){
     $("#localstrg-text").show();
 }
 
-showText();
+hideText();
+
+
+
+function currentTimeEl() {
+    $(".time-block").each(function() {
+        var currentHourEl = parseInt($(this).attr('id'), 10);
+        console.log(currentHourEl);
+        if (currentHourEl < currentHour){
+            console.log("past")
+            $(this).children().eq(1).addClass("past");
+        } else if (currentHourEl == currentHour){
+            console.log("present")
+            $(this).children().eq(1).removeClass('past');
+            $(this).children().eq(1).removeClass('textarea');
+            $(this).children().eq(1).addClass('present');
+        } else {
+            console.log("future")
+            $(this).children().eq(1).removeClass('past');
+            $(this).children().eq(1).removeClass('present');
+            $(this).children().eq(1).addClass('future');
+        }
+        });
+        
+}
+currentTimeEl();
