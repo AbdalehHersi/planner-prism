95c598236f08ce0d0abf5b09c8e805120ccf3216
Abdaleh Hersi <therealhersi03@gmail.com>
2022-08-02 13:29:42 +0100

    added local storage updater

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ $("button").on("click", function(){
     setTimeout(hideText, 3000);
     var userText = $(this).siblings("textarea").val();
     var userTimeKey = parseInt($(this).parent().attr('id'), 10);
+    console.log(userText, userTimeKey);
     localStorage.setItem(userTimeKey, userText);
 });
 
@@ -35,7 +36,7 @@ function currentTimeEl() {
         } else if (currentHourEl == currentHour){
             console.log("present")
             $(this).children().eq(1).removeClass('past');
-            $(this).children().eq(1).removeClass('textarea');
+            // $(this).children().eq(1).removeClass('textarea');
             $(this).children().eq(1).addClass('present');
         } else {
             console.log("future")
@@ -45,5 +46,15 @@ function currentTimeEl() {
         }
         });
         
-}
+};
 currentTimeEl();
+
+$("#9 .textarea").val(localStorage.getItem("9"));
+$("#10 .textarea").val(localStorage.getItem("10"));
+$("#11 .textarea").val(localStorage.getItem("11"));
+$("#12 .textarea").val(localStorage.getItem("12"));
+$("#13 .textarea").val(localStorage.getItem("13"));
+$("#14 .textarea").val(localStorage.getItem("14"));
+$("#15 .textarea").val(localStorage.getItem("15"));
+$("#16 .textarea").val(localStorage.getItem("16"));
+$("#17 .textarea").val(localStorage.getItem("17"));
